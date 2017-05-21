887496b3e23ea9ecc977e5c7030ca192ad3523f7
Guilherme Minglini <guilherme.minglini@gmail.com>
2017-05-21 12:16:23 -0300

    install SwiftMaskTextfield

---
+++ UINavigationController-Pod/Pods/Target Support Files/Pods-UINavigationController/Pods-UINavigationController-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_UINavigationControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UINavigationControllerVersionString[];
+
---
+++ UINavigationController-Pod/Pods/Target Support Files/Pods-UINavigationControllerUITests/Pods-UINavigationControllerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UINavigationControllerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UINavigationControllerUITests
+@end
